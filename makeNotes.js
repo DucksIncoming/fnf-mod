9c6426fa2b5f1b15dedc503f40c31e6a528f7154
John :v <60165941+johndoesstuff@users.noreply.github.com>
2021-04-01 13:42:05 -0400

    note editor stuff

---
+++ makeNotes.js
@@ -0,0 +1,13 @@
+setBpm(103);
+setSong("Fnaf1");
+setSpeed(1);
+clearSection();
+addNote(4, 1, 0);
+addNote(1, 2, 0);
+addNote(1, 3, 0);
+addNote(1, 4, 0);
+addNote(1, 1, 0);
+addNote(1, 2, 0);
+addNote(1, 3, 0);
+addNote(1, 4, 0);
+exportSection();
--- script for the note editor thats not shit.js
+++ script for the note editor thats not shit.js
@@ -1,1 +1,38 @@
 var msFromNote = (beat, bpm) => beat*6e4/bpm;
+var addSection = 
+window.song = {
+	song: {
+		bpm: undefined,
+		needsVoices: !0,
+		notes: [],
+		player1: "bf",
+		player2: "dad",
+		sectionLengths: [],
+		sections: 0,
+		song: undefined,
+		speed: 1,
+	}
+}
+var setBpm = (e) => {window.song.song.bpm = e;}
+var setSpeed = (e) => {window.song.song.speed = e;}
+var setSong = (e) => {window.song.song.song = e;}
+var clearSection = () => {
+	window.section = {
+		altAnim: false,
+		bpm: window.song.song.bpm,
+		changeBPM: false,
+		lengthInSteps: 0,
+		mustHitSection: true,
+		typeOfSection: 0,
+		sectionNotes: [],
+	}
+}
+window.globalTimeStep = 0;
+var addNote = (beatsFromLastNote, type, lengthInBeats) => {
+	window.globalTimeStep += msFromNote(beatsFromLastNote, window.song.song.bpm);
+	window.section.sectionNotes.push([window.globalTimeStep, type, msFromNote(lengthInBeats, window.song.song.bpm)]);
+}
+var exportSection = () => {
+	window.song.song.notes.push(window.section);
+	clearSection();
+}
